6a2debda30e85555450ba6416fc248b61e75d208
ryanavrilputra <ezzadut@gmail.com>
2025-01-21 18:25:29 +0700

    commit collection mongodb

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [CourseController::class, 'index'])->name('index');
 
 Route::resource('courses', CourseController::class)->except(['create', 'edit', 'show']);
-
-
-
 Route::resource('categories', CategoryController::class)->except(['create', 'edit', 'show']);
-
-
-
-
-
 Route::resource('instructors', InstructorController::class)->except(['create', 'edit', 'show']);
